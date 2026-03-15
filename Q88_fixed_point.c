994112c1924645f9cc71a60a88d122e3882e8d25
ye-soling <nys20600@gmail.com>
2026-03-15 17:47:36 +0900

    int 수정

--- Q88_fixed_point.c
+++ Q88_fixed_point.c
@@ -8,7 +8,8 @@ int main() {
     scanf("%lf", &input);
     fixed = (short)(input * 256);
 
-    for(int i = 15; i >= 0; i--) {
+    int i;
+    for(i = 15; i >= 0; i--) {
         printf("%d", (fixed >> i) & 1);
 
         if(i == 8) printf(".");
