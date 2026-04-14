4b531c4e8280f25f01f3a0fbeabdc1cfa6fcc4e5
ye-soling <nys20600@gmail.com>
2026-04-14 10:05:36 +0900

    과제(8) 1차 커밋

---
+++ fib/fib.c
@@ -0,0 +1,47 @@
+#include <stdio.h>
+#include <time.h>
+
+// 순환적 피보나치
+long long fibonacci_iter(int n) {
+    if (n == 1 || n == 2) return 1;
+    long long f1 = 1, f2 = 1, f3;
+    for (int i = 3; i <= n; i++) {
+        f3 = f1 + f2;
+        f1 = f2;
+        f2 = f3;
+    }
+    return f2;
+}
+
+// 재귀적 피보나치
+long long fibonacci_rec(int n) {
+    if (n == 1 || n == 2) return 1;
+    return fibonacci_rec(n - 1) + fibonacci_rec(n - 2);
+}
+
+int main() {
+    int N;
+    printf("정수(N)을 입력하세요: ");
+    scanf("%d", &N);
+
+    printf("n | fib| iter_time | rec_time\n");
+
+    for (int i = 1; i <= N; i++) {
+
+        // 순환
+        clock_t start_iter = clock();
+        long long fib_iter = fibonacci_iter(i);
+        clock_t end_iter = clock();
+        double time_iter = (double)(end_iter - start_iter) / CLOCKS_PER_SEC;
+
+        // 재귀
+        clock_t start_rec = clock();
+        long long fib_rec = fibonacci_rec(i);
+        clock_t end_rec = clock();
+        double time_rec = (double)(end_rec - start_rec) / CLOCKS_PER_SEC;
+
+        printf("%d | %lld | %.6f | %.6f\n", i, fib_iter, time_iter, time_rec);
+    }
+
+    return 0;
+}
